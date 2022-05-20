bca2b1672cf5efe66bb649051c9e59be655b6fb3
Khadiza <khadizaakhi756@gmail.com>
2022-05-21 00:10:06 +0600

    switch case, match expression

---
+++ switch.php
@@ -0,0 +1,33 @@
+<?php
+
+$paymentStatus = false;
+
+    switch($paymentStatus){
+
+    case 1:
+    echo 'Paid';
+    break;
+    
+
+        case 2:
+        case 3:
+        echo 'Payment Declimed';
+        break;
+
+            case 0:
+            echo 'Pending Payment';
+            break;
+
+                default:
+                echo 'Unknown Payment Status';
+            }
+            // echo '<br />';
+
+            $paymentStatusDisplay = match($paymentStatus){
+             1 >2 => 'Paid',
+             2,3 => 'Payment Declimed' ,
+             0 => 'Pending Payment' , 
+             default => 'Unknown Payment Status',
+            };
+            echo $paymentStatusDisplay;
+            ?>
